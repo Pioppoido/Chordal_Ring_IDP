caa718f2332ee907fdee6d57091c3b0f63a4aa6b
vagrant <vagrant@localhost.localdomain>
2020-11-11 07:00:50 +0900

    create new file

--- CRN3d2.c
+++ CRN3d2.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#define N 13
-#define d2 6
+#define N 12
+#define d2 4
 
 void construct1(int v,int Paths[N / 2 + 1][7][N]);
 
 int main(void){
   int i,j,k,v = 0;
   int P[N / 2 + 1][7][N] = {0};
+
+  printf("CR(%d,3,%d)\n",N,d2);
 
   for(v = 1;N / 2 + 1 > v;v++){
     construct1(v,P);
